c7fc7ea0bfe10893c7ee19932ce237a73204381b
Solomon McKInney <smckinney@wlapcs.org>
2017-02-28 21:04:10 +0000

    hugger bot

--- sketch.js
+++ sketch.js
@@ -4,15 +4,27 @@ function setup(){
 }
 
 function draw(){
-    line(35,20,400,200);
+    
+    line(200,120,275,160);
+    line(400,120,325,160);
     
     stroke(255,255,255);
     fill(255,0,0);
-    rect(290,100,250,250);
- 
-    stroke(0,0,0);    
-    strokeWeight(4);
+    rect(250,30,100,100);
+    rect(275,130,50,80);
+    
+    strokeWeight(2);
     fill(0,0,255);
-    ellipse(50,250,80,80);
+    stroke(0,0,0);
+    line(300,75,300,90);
+    ellipse(277,60,25,25);
+    ellipse(323,60,25,25);
+    line(277,110,323,110);
+
 
 }
+
+//draw mo 2 ellipses
+//draw me 2 rectangles
+//draw me 4 lines (1 with a positive slope, 1 with a negative slope)
+//1 with a slope of 0, and 1 with a slope of undefined.
