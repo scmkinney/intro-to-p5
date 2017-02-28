9f0d2601d819210e59cf60205a3457574a783cb7
Solomon McKInney <smckinney@wlapcs.org>
2017-02-28 20:16:02 +0000

    first commit

---
+++ sketch.js
@@ -0,0 +1,18 @@
+function setup(){
+    createCanvas(600,400);
+    background(100,100,100);
+}
+
+function draw(){
+    line(35,20,400,200);
+    
+    stroke(255,255,255);
+    fill(255,0,0);
+    rect(290,100,250,250);
+ 
+    stroke(0,0,0);    
+    strokeWeight(4);
+    fill(0,0,255);
+    ellipse(50,250,80,80);
+
+}
